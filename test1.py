5c327cad18ceac60427741d0661af94f72529337
raju8258 <raju8258@gmail.com>
2021-10-15 02:15:12 +0530

    second modification committed for push

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
 print("Hi, Hello world")
 print("this is my first learning excersise")
 print("successfully cloned to my local system from github")
+print("this is my 2nd modification after cloning")
